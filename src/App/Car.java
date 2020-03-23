2ee394ced35304c949fb9f09503cc4e6deeded11
BlakLeon <lefpoge@gmail.com>
2020-03-23 21:06:05 +0200

    Default constructor added

--- src/App/Car.java
+++ src/App/Car.java
@@ -46,4 +46,6 @@ public class Car {
     }
 
 
+    public Car() {
+    }
 }
--- src/App/Main.java
+++ src/App/Main.java
@@ -3,6 +3,8 @@ package App;
 public class Main {
     public static void main(String[] args) {
 
+        Car c1 = new Car();
+
        // System.out.println("Lef");
 
     }
